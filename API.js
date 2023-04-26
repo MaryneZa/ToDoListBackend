ef3db9cf2a0c5c7034bac0267a14a3bbfaaef92e
MaryneZa <maryne6916@gmail.com>
2023-04-26 13:19:18 +0700

    Backend failed

--- API.js
+++ API.js
@@ -1,7 +1,12 @@
 // import connection from './DB';
+const axios = require('axios');
 const connection = require('./DB')
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const api = axios.create({
+  baseURL: 'https://localhost:3000'
+})
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,6 +20,26 @@ app.get('/user',(req, res)=>{
     
 })
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.post('/register', (req, res) => {
+  const {username, name, password } =  req.body;
+
+  api.post('/register', {username, name, password})
+  .then(response => {
+    res.json(response.data);
+    connection.query(`INSERT INTO user(User_Id, User_password, Name) VALUES ('${username}', '${password}', '${name}')`, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send('ERROR OCCURRED WHILE REGISTERING !!');
+      } else {
+        res.send('REGISTERED SUCCESSFUL !!');
+      }
+    });
+  })
+  .catch(error => {
+    res.status(500).json({ error: 'Internal server error' });
+  });
+});
+
+app.listen(6951, () => {
+  console.log('Server listening on port 6951');
 });
